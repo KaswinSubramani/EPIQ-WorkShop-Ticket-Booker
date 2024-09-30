cfa396c1248345be4c6f31a626089e97c6897e0b
Kaswin Subramani <128113768+KaswinSubramani@users.noreply.github.com>
2024-09-30 21:02:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
@@ -15,8 +14,13 @@ public class Main {
         workshop1.add(new Commerce("All About Tally & Excel","Karpagam Academy Of Higher Education",201,21,299,"12/09/2024"));
         workshop1.add(new Commerce("Basics of Commerce","PSG Arts and Science College",202,66,499,"12/09/2024"));
         workshop1.add(new Commerce("Spectrum A complete Guide","Sri Krishna Arts and Science college",204,17,349,"12/09/2024"));
+        workshop2.add(new management("Basics to Stock Market","Sri Krishna Arts and Science college",203,29,349,"12/09/2024"));
+        workshop2.add(new management("All About Tally & Excel","Karpagam Academy Of Higher Education",201,21,299,"12/09/2024"));
+        workshop2.add(new management("Basics of Commerce","PSG Arts and Science College",202,66,499,"12/09/2024"));
+        workshop2.add(new management("Spectrum A complete Guide","Sri Krishna Arts and Science college",204,17,349,"12/09/2024"));
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\"EPIQ Workshop Ticket Booker\"\t\t\t\t\t\t\t\t\t\t\t");
         String name = "";
+        int workshopNo,count;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter Your Name : ");
         name = s.next();
@@ -33,24 +37,71 @@ public class Main {
                     for (CS i : workshop) {
                         i.display();
                     }
-                    booking book = new booking();
+                    System.out.print("Enter the workshop number : ");
+                    workshopNo = s.nextInt();
+                    System.out.print("Enter the members Count : ");
+                    count = s.nextInt();
+                    booking book = new booking(workshopNo,count);
                     if (book.isavailable(workshop, booking)) {
-                        booking.add(book);
-                        System.out.println("Your Booking is Comfirmed");
+                        System.out.println("The Total price : Rs:"+book.price);
+                        System.out.print("Would you like to proceed further (Yes/No) : ");
+                        String st = s.next();
+                        if(st.equalsIgnoreCase("yes")) {
+                            booking.add(book);
+                            System.out.println("Your Booking is Comfirmed");
+                        }else if(st.equalsIgnoreCase("no")){
+                            return;
+                        }
                     } else {
-                        System.out.println("Sorry,You could not book the tickets");
+                        System.out.println("Sorry,The tickets are not available");
                     }
                 }else if(stream==2){
                     for (Commerce i : workshop1) {
                         i.display();
                     }
-                    booking book = new booking();
+                    System.out.print("Enter the workshop number : ");
+                    workshopNo = s.nextInt();
+                    System.out.print("Enter the members Count : ");
+                    count = s.nextInt();
+                    booking book = new booking(workshopNo,count);
                     if (book.isavailable1(workshop1, booking)) {
-                        booking.add(book);
-                        System.out.println("Your Booking is Comfirmed");
+                        System.out.println("The Total price : Rs:"+book.price);
+                        System.out.print("Would you like to proceed further (Yes/No) : ");
+                        String st = s.next();
+                        if(st.equalsIgnoreCase("yes")) {
+                            booking.add(book);
+                            System.out.println("Your Booking is Comfirmed");
+                        }else if(st.equalsIgnoreCase("no")){
+                            return;
+                        }
                     } else {
-                        System.out.println("Sorry,You could not book the tickets");
+                        System.out.println("Sorry, The tickets are not availabe");
                     }
+                }else if(stream==3){
+                    for (management i : workshop2) {
+                        i.display();
+                    }
+                    System.out.print("Enter the workshop number : ");
+                    workshopNo = s.nextInt();
+                    System.out.print("Enter the members Count : ");
+                    count = s.nextInt();
+                    booking book = new booking(workshopNo,count);
+                    if (book.isavailable2(workshop2, booking)) {
+                        System.out.println("The Total price : Rs:"+book.price);
+                        System.out.print("Would you like to proceed further (Yes/No) : ");
+                        String st = s.next();
+                        if(st.equalsIgnoreCase("yes")) {
+                            booking.add(book);
+                            System.out.println("Your Booking is Comfirmed");
+                        }else if(st.equalsIgnoreCase("no")){
+                            return;
+                        }
+                    } else {
+                        System.out.println("Sorry, The tickets are not availabe");
+                    }
+                }else{
+                    System.out.println("Please Choose (1 to 3) : ");
+                    continue;
                 }
             System.out.print("Would you like to book extra workshop tickets (Yes/No) : ");
             x = s.next();
