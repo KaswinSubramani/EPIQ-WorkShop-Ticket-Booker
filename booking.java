002ffb963d7ec600080225e6d568d43f27994ec7
Kaswin Subramani <128113768+KaswinSubramani@users.noreply.github.com>
2024-09-30 21:49:28 +0530

    Update booking.java

--- booking.java
+++ booking.java
@@ -1,4 +1,8 @@
+import java.util.*;
 public class booking {
+    String workName;
+    String CollegeName;
+    String date;
     private int workshopNo;
     private int count;
     public int getWorkshopNo() {
@@ -7,6 +11,9 @@ public class booking {
     public booking(int workshopNo,int count){
         this.workshopNo = workshopNo;
         this.count = count;
+    }
+    public int Count(){
+        return this.count;
     }
     int c=0,price=1,capacity=0;
     public boolean isavailable(ArrayList<CS> workshop,ArrayList<booking> booking){
@@ -53,6 +60,33 @@ public class booking {
             }
         }
         return c <= capacity ? true : false;
+    }
+    public void bookedDetails(ArrayList<CS> workshop){
+        for(CS i:workshop){
+            if(i.getBooking()==workshopNo){
+                this.workName = i.workName;
+                this.CollegeName = i.CollegeName;
+                this.date = i.date;
+            }
+        }
+    }
+    public void bookedDetails1(ArrayList<Commerce> workshop){
+        for(Commerce i:workshop){
+            if(i.getBooking()==workshopNo){
+                this.workName = i.workName;
+                this.CollegeName = i.CollegeName;
+                this.date = i.date;
+            }
+        }
+    }
+    public void bookedDetails2(ArrayList<management> workshop){
+        for(management i:workshop){
+            if(i.getBooking()==workshopNo){
+                this.workName = i.workName;
+                this.CollegeName = i.CollegeName;
+                this.date = i.date;
+            }
+        }
     }
 
 }
